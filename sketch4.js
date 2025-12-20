7687a2b933c10b447ed74b68ccb543f7d52e6dd7
skyash-dev <yashpc09@gmail.com>
2025-12-20 19:18:07 +0530

    sketch 5

--- sketch4.js
+++ sketch4.js
@@ -1,53 +1,34 @@
+// custom variables for y-coordinate of sun and horizon.
+let sunHeight;
+let horizon = 200;
+
 function setup() {
-  // creates a canvas 400px wide and 400px high.
   createCanvas(400, 400);
 }
 
-let cloudOneX = 50;
-let speed = 1;
-//custom variable for shooting stars
-let lineXone = 0;
-let lineYone = 0;
-
 function draw() {
-  frameRate(15);
-  background(0, 0, 255);
-  //displays the x and y position of the mouse on the canvas
-  fill(255); //white text
-  text(`${mouseX}, ${mouseY}, ${cloudOneX}`, 20, 20);
+  background(0);
 
-  stroke(0);
-  strokeWeight(1);
-  fill("gray");
-  triangle(-40, 300, 75, 100, 200, 300);
-  triangle(100, 300, 300, 100, 500, 300);
+  // sun follows y-coordinates of mouse
+  sunHeight = mouseY;
 
-  fill("green");
-  rect(0, 300, 400, 100);
-
-  strokeWeight(0);
-  fill("white");
-  circle(350, 50, 100);
-  strokeWeight(0);
-  fill("blue");
-  circle(320, 50, 100);
-
-  if (cloudOneX > 320) {
-    speed = -1;
-  } else if (cloudOneX == 50) {
-    speed = 1;
+  if (sunHeight < horizon) {
+    background("lightblue");
   }
-  cloudOneX += speed;
-  //cloud
-  fill(255);
-  ellipse(cloudOneX, 40, 80, 40);
-  ellipse(cloudOneX + 60, 80, 80, 40);
 
-  //set shooting star to random location
-  lineXone = random(0, width);
-  lineYone = random(0, height / 2);
+  // sun
+  fill("yellow");
+  circle(200, sunHeight, 160);
 
-  strokeWeight(1);
-  stroke("yellow");
-  line(lineXone, lineYone, lineXone + 30, lineYone - 30);
+  // draw line for horizon
+  stroke("green");
+  line(0, horizon, 400, horizon);
+
+  // grass
+  if (sunHeight < horizon) {
+    fill("lightgreen");
+  } else {
+    fill("green");
+  }
+  rect(0, horizon, 400, 200);
 }
